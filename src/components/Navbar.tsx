537a75131afc4b36e4b4a617a7833dd557eae31b
UnionIM <dam.rowv@gmail.com>
2023-03-02 12:08:28 +0200

    Usage isAuth to display different information, create item button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,50 @@
 import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import BonamiController from "../controllers/BonamiController";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const logOutHandler = () => {
-    window.open("http://localhost:5000/logout");
+    BonamiController.logOut()
+      .then((res: { message: string }) => {
+        if (res.message === "success") {
+          window.location.href = "http://localhost:3000/login";
+        }
+      })
+      .catch((e) => console.log(e));
+    localStorage.clear();
   };
+
   return (
-    <nav>
-      <Link to={"/"}>Home</Link>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to={"/"} style={{ flexGrow: 1 }}>
+            <Typography component={"span"}>Bonami Admin</Typography>
+          </Link>
+          {localStorage.getItem("isAuth") ? (
+            <div>
+              <Link to={"/item/create"}>
+                <Button sx={{ mr: 5 }} startIcon={<Add />} color={"inherit"}>
+                  <Typography sx={{ fontSize: 14 }} component={"span"}>
+                    Create item
+                  </Typography>
+                </Button>
+              </Link>
+              <Button onClick={logOutHandler} color={"inherit"}>
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </Toolbar>
+        {/*<Link to={"/"}>Home</Link>
       <Link to={"/login"}>Login</Link>
-      <button onClick={logOutHandler}>LogOut</button>
-    </nav>
+      <button onClick={logOutHandler}>LogOut</button>*/}
+      </AppBar>
+    </Box>
   );
 };
 
