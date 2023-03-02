90f265ff84fe27fc42c8742be49761758618caaf
UnionIM <dam.rowv@gmail.com>
2023-03-02 12:01:34 +0200

    Global styles, isAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./router/AppRouter";
 import Navbar from "./components/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createGlobalStyle } from "styled-components";
+import BonamiController from "./controllers/BonamiController";
+import { Box } from "@mui/material";
 
 function App() {
+  const Global = createGlobalStyle`
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    a{
+      text-decoration: none;
+      color: unset;
+    }
+  `;
+  useEffect(() => {
+    BonamiController.isAuth()
+      .then((res: { isAuth: boolean }) => {
+        if (res.isAuth) {
+          localStorage.setItem(`isAuth`, JSON.stringify(true));
+        }
+      })
+      .catch((e) => {
+        if (e.response.status === 401) {
+          localStorage.clear();
+        }
+      });
+  }, []);
+  const theme = createTheme({
+    typography: {
+      fontFamily: `Roboto`,
+      fontSize: 16,
+      fontWeightRegular: 400,
+      fontWeightBold: 700,
+    },
+  });
   return (
-    <div className="App">
-      <Navbar />
-      <AppRouter />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Global />
+      <Box bgcolor={"#f1f1f1"} height={"100vh"}>
+        <Navbar />
+        <AppRouter />
+      </Box>
+    </ThemeProvider>
   );
 }
 
