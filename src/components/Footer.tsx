0d5c5a2503479683011358d006af05cabd37e595
UnionIM <dam.rowv@gmail.com>
2023-03-06 17:44:33 +0200

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { colorful } from "../design/colors";
+
+const Footer = () => {
+  return <Box height={"180px"} bgcolor={colorful.blue}></Box>;
+};
+
+export default Footer;
