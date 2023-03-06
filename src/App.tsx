20e58df540e8edee6dd50739988f74bbb009915c
UnionIM <dam.rowv@gmail.com>
2023-03-06 17:51:27 +0200

    Deleted MyCard, rewrote MuiCard, MuiFormControl, MuiTextField, MuiOutlinedInput themes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { createGlobalStyle } from "styled-components";
 import BonamiController from "./controllers/BonamiController";
 import { Box } from "@mui/material";
+import { gray } from "./design/colors";
+import Footer from "./components/Footer";
 
 function App() {
   const Global = createGlobalStyle`
@@ -38,13 +40,63 @@ function App() {
       fontWeightRegular: 400,
       fontWeightBold: 700,
     },
+    components: {
+      MuiCard: {
+        styleOverrides: {
+          root: {
+            padding: "25px",
+            borderRadius: "8px",
+            backgroundColor: "white",
+            boxShadow: `0px 0px 5px ${gray.dark}`,
+            display: "inline-block",
+          },
+        },
+      },
+      MuiFormControl: {
+        styleOverrides: {
+          root: {
+            ".MuiFormLabel-root": {
+              top: "-4px",
+            },
+          },
+        },
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            ".MuiInputBase-input": {
+              padding: "0",
+            },
+            ".MuiInputBase-root": {
+              padding: "0",
+            },
+            ".MuiOutlinedInput-input": {
+              padding: "11px 17px",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            ".MuiInputBase-input": {
+              padding: "0",
+            },
+            ".MuiOutlinedInput-input": {
+              padding: "11px 17px",
+            },
+          },
+        },
+      },
+    },
   });
   return (
     <ThemeProvider theme={theme}>
       <Global />
-      <Box bgcolor={"#f1f1f1"} height={"100vh"}>
+      <Box bgcolor={"#f1f1f1"} height={"100%"}>
         <Navbar />
         <AppRouter />
+        <Footer />
       </Box>
     </ThemeProvider>
   );
