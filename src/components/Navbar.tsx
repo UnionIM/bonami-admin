1c7a469780c36ae2354ea65d4b43e9a74f187c29
UnionIM <dam.rowv@gmail.com>
2023-03-07 16:45:40 +0200

    DropDown menu, create category dialog

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import { Add } from "@mui/icons-material";
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Button, Box, Grid } from "@mui/material";
 import BonamiController from "../controllers/BonamiController";
+import { Add } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import DropDownMenu from "./UI/DropDownMenu";
+import ModalDialog from "./UI/ModalDialog";
+import MultiLangInputs from "./UI/Inputs/MultiLangInputs";
+import { useForm } from "react-hook-form";
+import { ICreateCategoryForm } from "../models/bonami-client";
 
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const { register, handleSubmit } = useForm<ICreateCategoryForm>();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   const logOutHandler = () => {
     BonamiController.logOut()
       .then((res: { message: string }) => {
@@ -24,25 +37,48 @@ const Navbar = () => {
             <Typography component={"span"}>Bonami Admin</Typography>
           </Link>
           {localStorage.getItem("isAuth") ? (
-            <div>
-              <Link to={"/item/create"}>
-                <Button sx={{ mr: 5 }} startIcon={<Add />} color={"inherit"}>
-                  <Typography sx={{ fontSize: 14 }} component={"span"}>
-                    Create item
-                  </Typography>
-                </Button>
-              </Link>
+            <Grid container sx={{ width: "unset" }} gap={"25px"}>
+              <DropDownMenu buttonContent={<Add />}>
+                <Link to={"/item/create"} key={1}>
+                  Create item
+                </Link>
+                <span onClick={handleClickOpen} key={2}>
+                  Create category
+                </span>
+              </DropDownMenu>
               <Button onClick={logOutHandler} color={"inherit"}>
                 Logout
               </Button>
-            </div>
+            </Grid>
           ) : (
             <div></div>
           )}
+          <ModalDialog
+            state={open}
+            setState={setOpen}
+            title={"Create category"}
+            width={"500px"}
+          >
+            <form
+              onSubmit={handleSubmit((data) => {
+                console.log(data);
+              })}
+            >
+              <MultiLangInputs
+                register={register}
+                header={"Name"}
+                registerName={"name"}
+              />
+              <Button
+                type={"submit"}
+                variant="contained"
+                sx={{ width: "100%", marginTop: "20px" }}
+              >
+                CREATE
+              </Button>
+            </form>
+          </ModalDialog>
         </Toolbar>
-        {/*<Link to={"/"}>Home</Link>
-      <Link to={"/login"}>Login</Link>
-      <button onClick={logOutHandler}>LogOut</button>*/}
       </AppBar>
     </Box>
   );
