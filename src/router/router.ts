90363a7cb1accf2150961611e48d42f530ea29cc
UnionIM <dam.rowv@gmail.com>
2023-03-20 13:58:42 +0200

    Order page, order list page

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,14 +3,16 @@ import Home from "../pages/Home";
 import CreateItem from "../pages/CreateItem";
 import ItemList from "../pages/ItemList";
 import OrderList from "../pages/OrderList";
-import ItemEdit from "../pages/ItemEdit";
+import EditItem from "../pages/EditItem";
+import OrderPage from "../pages/OrderPage";
 
 export const privateRoutes = [
   { path: "/", component: Home },
   { path: "/item/create", component: CreateItem },
   { path: "/item/list/:page", component: ItemList },
-  { path: "/order/list", component: OrderList },
-  { path: "/item/edit/:id", component: ItemEdit },
+  { path: "/item/edit/:id", component: EditItem },
+  { path: "/order/list/:page", component: OrderList },
+  { path: "/order/:id", component: OrderPage },
 ];
 
 export const publicRoutes = [{ path: "login", component: Login }];
