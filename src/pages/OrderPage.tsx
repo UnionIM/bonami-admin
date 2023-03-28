ba37c8db0d22d3fe8d9cd563066a1065690d8def
Danylo Honcharov <Dan.rowv@gmail.com>
2023-03-28 10:10:33 +0300

    Order page

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -4,6 +4,7 @@ import useFetchData from "../hooks/useFetchData";
 import BonamiController from "../controllers/BonamiController";
 import {
   Box,
+  Button,
   Card,
   CircularProgress,
   Divider,
@@ -12,8 +13,14 @@ import {
 } from "@mui/material";
 import MyAlert from "../components/UI/MyAlert";
 import { IAlertState } from "../models/bonami-client";
-import person from "../design/svg/Person.svg";
-import { colorful, gray } from "../design/colors";
+import { gray } from "../design/colors";
+import CustomerInformation from "../components/CustomerInformation";
+import OrderStatus from "../components/UI/OrderStatus";
+import { timeConverter } from "../utils/timeConverter";
+import ItemInOrderList from "../components/ItemInOrderList";
+import cross from "../design/svg/Cross.svg";
+import check from "../design/svg/Check.svg";
+import { IOrderedItem } from "../models/bonami-server-response";
 
 const OrderPage = () => {
   const { id } = useParams();
@@ -24,11 +31,11 @@ const OrderPage = () => {
     severity: "error",
   });
 
-  const {
-    data: order,
-
-    message,
-  } = useFetchData(BonamiController.getOrderById, [id], [id]);
+  const { data: order, message } = useFetchData(
+    BonamiController.getOrderById,
+    [id],
+    [id]
+  );
 
   useEffect(() => {
     //@ts-ignore
@@ -48,59 +55,69 @@ const OrderPage = () => {
     }
   }, [message]);
 
+  const getTotalPrice = (items: IOrderedItem[]) => {
+    return items.reduce((acc, val) => acc + val.price * val.amount, 0);
+  };
+
   return (
-    <Box p={"32px"}>
-      {order && !order.message ? (
-        <>
-          <Card sx={{ width: "371px" }}>
-            <Grid container justifyContent={"space-between"}>
-              <Grid
-                container
-                flexDirection={"column"}
-                sx={{ width: "unset" }}
-                gap={"20px"}
-              >
-                <Typography fontWeight={700}>Customer</Typography>
-                <Grid item>
-                  <Typography>
-                    {order.name.firstName} {order.name.secondName}{" "}
-                    {order.name.patronymic}
-                  </Typography>
-                  <Typography>
-                    {order.amountOfOrders === 1
-                      ? `${order.amountOfOrders} order`
-                      : `${order.amountOfOrders} orders`}
-                  </Typography>
+    <Box p={"32px"} display={"flex"}>
+      <div style={{ margin: "0 auto" }}>
+        {order && !order.message ? (
+          <>
+            <Grid container gap={"25px"}>
+              <Card>
+                <Grid
+                  container
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                >
+                  <Grid item>
+                    <Grid container alignItems={"center"} gap={"15px"}>
+                      <OrderStatus status={order.status} />
+                      <Typography
+                        fontWeight={700}
+                        sx={{ textTransform: "capitalize" }}
+                      >
+                        {order.status}
+                      </Typography>
+                    </Grid>
+                    <Typography color={gray.dark} sx={{ mt: "10px" }}>
+                      {timeConverter(order.createdAt)}
+                    </Typography>
+                  </Grid>
+                  <Grid container gap={"10px"} sx={{ width: "unset" }}>
+                    <Button variant="contained">MARK AS DELIVERED</Button>
+                    <Button variant="contained">MARK AS CANCELED</Button>
+                  </Grid>
+                </Grid>
+                <Divider sx={{ borderColor: gray.default, margin: "25px 0" }} />
+                <ItemInOrderList items={order.items}></ItemInOrderList>
+              </Card>
+              <CustomerInformation order={order} />
+            </Grid>
+            <Card sx={{ mt: "25px", width: "655px" }}>
+              <Grid container alignItems={"center"} gap={"12px"}>
+                <img src={order.isPaid ? check : cross} alt="" />
+                <Typography fontWeight={700}>
+                  {order.isPaid ? "Paid" : "Not paid"}
+                </Typography>
+                <Grid
+                  container
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                  sx={{ width: "84%", textAlign: "right" }}
+                >
+                  <Typography>Total</Typography>
+                  <Typography>{getTotalPrice(order.items)}</Typography>
                 </Grid>
               </Grid>
-              <Grid
-                container
-                flexDirection={"column"}
-                alignItems={"center"}
-                justifyContent={"space-between"}
-                sx={{ width: "unset" }}
-              >
-                <Box
-                  borderRadius={"50%"}
-                  bgcolor={
-                    order.isAuthenticated ? colorful.green : colorful.red
-                  }
-                  sx={{ width: "50px", height: "50px", padding: "10px 12px" }}
-                >
-                  <img src={person} alt="" />
-                </Box>
-                <Typography>
-                  {order.isAuthenticated ? "Have acc." : "No accnt."}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider sx={{ borderColor: gray.default, margin: "25px 0" }} />
-          </Card>
-        </>
-      ) : (
-        <CircularProgress />
-      )}
-      <MyAlert state={openSnackBar} setState={setOpenSnackBar} />
+            </Card>
+          </>
+        ) : (
+          <CircularProgress />
+        )}
+        <MyAlert state={openSnackBar} setState={setOpenSnackBar} />
+      </div>
     </Box>
   );
 };
