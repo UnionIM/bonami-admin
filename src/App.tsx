12db7558360d5f72e1a58fa5cff995ecebe50d07
Danylo Honcharov <Dan.rowv@gmail.com>
2023-04-13 11:00:09 +0300

    Custom scroll

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,33 @@ import Footer from "./components/Footer";
 import { useNavigate } from "react-router-dom";
 
 const Global = createGlobalStyle`
-    *{
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    a{
-      text-decoration: none;
-      color: unset;
-    }
-  `;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+    color: unset;
+  }
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: white;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #B5B5B5;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #808080;
+  }
+`;
 
 function App() {
   const navigate = useNavigate();
